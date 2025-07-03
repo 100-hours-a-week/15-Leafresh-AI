d3f3dae026646829e4db380911325824170d35ad
kellyn(이가은) <103016025+kellyn0522@users.noreply.github.com>
2025-07-03 18:25:19 +0900

    테스트 결과 정리 및 모니터링 연결

--- Server1/main.py
+++ Server1/main.py
@@ -13,7 +13,13 @@ from router.censorship_router import router as censorship_router
 from fastapi.exceptions import RequestValidationError, HTTPException
 from router.censorship_router import validation_exception_handler, http_exception_handler
 
+from prometheus_client import start_http_server
+
 load_dotenv()
+
+# 모니터링 서버 실행 함수
+def run_metrics_server():
+    start_http_server(9101)
 
 # worker를 main 실행할 때 지속적으로 실행되도록 변경 
 # pubsub_v1이 동기로 실행되므로 async를 붙이지 않음 
@@ -21,6 +27,7 @@ load_dotenv()
 async def lifespan(app: FastAPI):               # app 인자를 받는 형태가 아니면 에러가 발생하므로 삭제 불가능 
     print("[DEBUG] Lifespan 시작됨")
     threading.Thread(target=run_worker, daemon=True).start()
+    threading.Thread(target=run_metrics_server, daemon=True).start()    # 메트릭 서버를 별도 스레드에서 실행 
     yield
 
 # app 초기화
